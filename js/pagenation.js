0709346ac88192e6d5be6948b69eef43f56d04c5
shimesava0306 <kunygtr0306@gmail.com>
2023-09-13 15:22:52 +0900

    reviewページ作成

--- js/pagenation.js
+++ js/pagenation.js
@@ -11,3 +11,16 @@ $(function () {
     })
 });
 
+$(function () {
+    $('.reviewAll').paginathing({//親要素のclassを記述
+        perPage: 20,//1ページあたりの表示件数
+        prevText: '前へ',//1つ前のページへ移動するボタンのテキスト
+        nextText: '次へ',//1つ次のページへ移動するボタンのテキスト
+        firstText: '<i class="fas fa-angle-double-left"></i>',
+        lastText: '<i class="fas fa-angle-double-right"></i>',
+        prevText: '<i class="fas fa-angle-left"></i>',
+        nextText: '<i class="fas fa-angle-right"></i>',
+        activeClass: 'active',
+    })
+});
+
